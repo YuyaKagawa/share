1280764c8ac2cc0b950806fcdde8f49f6ac5cc43
YuyaKagawa <kagawa@h176.251.238.10.1017424.vlan.kuins.net>
2018-10-13 14:47:13 +0900

    main.py updated

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from cal import sum_test
 from square import square
-a,b = list(map(int, input().split()))
-print('input', a, b)
+import sys
+
+args = sys.argv
+
+if len(args) < 3:
+	print("input as [python main.py a b]")
+	sys.exit()
+
+_, a, b = args
+a, b = list(map(int, [a, b]))
+
+print("a, b = {}, {}".format(a, b))
+
+"""
+a, b = list(map(int, input().split()))
+
+print('input = ', a, b)
+"""
+
 x = sum_test(a,b)
+
 print('plus', x)
+
 xx = square(x)
+
 print('square', xx)

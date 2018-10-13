dc27c0ea5e0d576e279041208fff60b25154c66d
mrkmakr <mrkmakr123@gmail.com>
2018-10-13 14:29:19 +0900

    rename

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cal import sum_test
 from square import square
 a,b = list(map(int, input().split()))
 print('input', a, b)
-x = add(a,b)
+x = sum_test(a,b)
 print('plus', x)
 xx = square(x)
 print('square', xx)
